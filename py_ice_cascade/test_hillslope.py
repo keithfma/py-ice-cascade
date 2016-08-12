f8abaa840f9a7bd99f3baf78487bcfc3712a5ef0
Keith Ma <keith.ma@bostonfusion.com>
2016-08-12 15:29:44 -0400

    Added skeleton for hillslope.py unit tests

--- py_ice_cascade/test_hillslope.py
+++ py_ice_cascade/test_hillslope.py
@@ -1,3 +1,23 @@
 """
 Unit tests for Python ICE-CASCADE hillslope erosion-deposition model component
 """
+
+import unittest
+
+class ftcs_openbnd_TestCase(unittest.TestCase):
+    """Tests for ftcs_openbnd model component"""
+
+    def test_init_args(self):
+        pass
+
+    def test_steady_1d(self):
+        pass
+
+    def test_steady_2d(self):
+        pass
+
+    def test_transient_1d(self):
+        pass
+
+    def test_transient_2d(self):
+        pass
