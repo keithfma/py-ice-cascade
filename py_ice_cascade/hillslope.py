9c6afa21449a83ece3a83aa7617408cc08e3f037
Keith Ma <keith.ma@bostonfusion.com>
2016-08-15 10:01:45 -0400

    Populates internal points of hillslope FTCS coeff matrix

--- py_ice_cascade/hillslope.py
+++ py_ice_cascade/hillslope.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-class ftcs_openbnd():
+class ftcs():
     """
     Hillslope diffusion model using forward-time center-space (FTCS) finite
     diffence scheme with "open" boundary conditions. 
@@ -26,6 +26,9 @@ class ftcs_openbnd():
     constant. 
     """
 
+    # NOTE: attributes and methods with the "_" prefix are considered private,
+    #       use outside the object at your own risk
+
     def __init__(self, height, delta, kappa):
         """
         Arguments:
@@ -33,22 +36,75 @@ class ftcs_openbnd():
             delta = Scalar double, grid spacing, assumed square, [m]
             kappa = Scalar double, diffusion coefficient, [m**2/a]
         """
-        
-        # declare attributes
-        self.height = None # elevation grid
-        self.delta = None  # grid spacing
-        self.kappa = None  # diffusion parameter
-        
-        # set attributes
-        self.delta = np.double(delta)
-        self.kappa = np.double(kappa) 
-        self.set_height(height) 
-        
+
+        self._height = None
+        self._delta = None
+        self._kappa = None
+        self._nx = None
+        self._ny = None
+
+        self.set_height(height)
+        self.set_diffusivity(kappa)
+        self._delta = np.double(delta)
+        self._set_coeff_matrix() # TODO: use BC names to adapt boundary coefficients
+
     def set_height(self, new):
-        self.height = np.atleast_2d(np.array(new, dtype=np.double))
+        """Set height grid internal attribute"""
+        new_array = np.double(new)
+        if new_array.ndim != 2:
+            print("hillslope: height is not a 2D array"); sys.exit()
+        if (self._height != None) and (new_array.shape != (self._ny, self._nx)):
+            print("hillslope: cannot change shape of height grid"); sys.exit()
+        self._ny, self._nx = new_array.shape
+        self._height = np.ravel(new_array, order='C')
 
     def get_height(self):
-        return self.height
+        """Return height grid as 2D numpy array"""
+        return self._height.reshape((self._ny, self._nx), order='C')
+
+    def set_diffusivity(self, new):
+        """Set diffusivity grid internal attribute"""
+        self._kappa = np.double(new)
+        if self._kappa.ndim != 2:
+            print("hillslope: diffusivity is not a 2D array"); sys.exit()
+        if self._kappa.shape != (self._ny, self._nx):
+            print("hillslope: diffusitity grid dims do not match height grid"); sys.exit()
+
+    def _set_coeff_matrix(self):
+        """Define sparse coefficient matrix for dHdt stencil"""
+        # NOTE: FTCS is a 5-point stencil, since diffusivity is a grid, all
+        # coefficients are potentially unique. The approach here is to compute
+        # coefficients for each diagonal as a matrix including BCs, then
+        # convert to a sparse penta-diagonal. Special care is needed to allow
+        # for the general case where some diagonals may wrap around.
+        
+        # declare coefficient arrays
+        i_j   = np.zeros((self._ny, self._nx), dtype = np.double)
+        im1_j = np.zeros((self._ny, self._nx), dtype = np.double)
+        ip1_j = np.zeros((self._ny, self._nx), dtype = np.double)
+        i_jm1 = np.zeros((self._ny, self._nx), dtype = np.double)
+        i_jp1 = np.zeros((self._ny, self._nx), dtype = np.double)
+
+        # populate coefficients for interior points
+        inv2delta2 = 1.0/(2.0*self._delta*self._delta)
+        kappa_i_j   = self._kappa[1:-1,1:-1] # just views, should be efficient
+        kappa_ip1_j = self._kappa[2:  ,1:-1]
+        kappa_im1_j = self._kappa[ :-2,1:-1]
+        kappa_i_jp1 = self._kappa[1:-1,2:  ]
+        kappa_i_jm1 = self._kappa[1:-1, :-2]
+
+        i_j[1:-1,1:-1] = -inv2delta2*(4.0*kappa_i_j + kappa_im1_j + kappa_ip1_j + kappa_i_jm1 + kappa_i_jp1)
+        im1_j[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_im1_j)
+        ip1_j[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_ip1_j)
+        i_jm1[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_i_jm1)
+        i_jp1[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_i_jp1)
+
+        # debug 
+        self.i_j = i_j
+        self.ip1_j = ip1_j
+        self.im1_j = im1_j
+        self.i_jp1 = i_jp1
+        self.i_jm1 = i_jm1
 
     def run(self, run_time):
         """
@@ -80,23 +136,26 @@ if __name__ == '__main__':
     ny = 100
     max_time = 5.0
     time_step = 0.05
-    h0 = np.random.rand(nx, ny).astype(np.double)-0.5
+    h0 = np.random.rand(ny, nx).astype(np.double)-0.5
     h0[:,0] = np.double(0.0) # constant values are compatible with "open" BC treatment
     h0[:,-1] = np.double(0.0)
     h0[0,:] = np.double(0.0)
     h0[-1,:] = np.double(0.0)
     dd = np.double(1.0)
-    kk = np.double(1.0)
-    model = ftcs_openbnd(h0, dd, kk)
-    # # update and plot model
-    plt.imshow(model.get_height(), interpolation='nearest', clim=(-0.5,0.5))
-    plt.colorbar()
-    plt.ion()
-    time = 0.0
-    while time < max_time: 
-        model.run(time_step)
-        time += time_step
-        plt.cla()
-        plt.imshow(model.get_height(), interpolation='nearest', clim=(-0.5,0.5))
-        plt.title("TIME = {:.2f}".format(time))
-        plt.pause(0.05)
+    kk = np.ones((ny, nx), dtype=np.double)
+    model = ftcs(h0, dd, kk)
+
+
+
+    # # # update and plot model
+    # plt.imshow(model.get_height(), interpolation='nearest', clim=(-0.5,0.5))
+    # plt.colorbar()
+    # plt.ion()
+    # time = 0.0
+    # while time < max_time: 
+    #     model.run(time_step)
+    #     time += time_step
+    #     plt.cla()
+    #     plt.imshow(model.get_height(), interpolation='nearest', clim=(-0.5,0.5))
+    #     plt.title("TIME = {:.2f}".format(time))
+    #     plt.pause(0.05)
