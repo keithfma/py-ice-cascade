415cf7c335666f983c3a06c6b9165daa5ec855bb
Keith Ma <keith.ma@bostonfusion.com>
2016-08-17 12:00:55 -0400

    Switched to direct population of coefficient matrix

--- py_ice_cascade/hillslope.py
+++ py_ice_cascade/hillslope.py
@@ -96,86 +96,15 @@ class ftcs():
     def _set_coeff_matrix(self):
         """Define sparse coefficient matrix for dHdt stencil"""
         # NOTE: FTCS is a 5-point stencil, since diffusivity is a grid, all
-        # coefficients are potentially unique. The approach here is to compute
-        # coefficients for each diagonal as a matrix including BCs, then
-        # convert to a sparse penta-diagonal. Special care is needed to allow
-        # for the general case where some diagonals may wrap around.
-        
-        # declare coefficient arrays
-        i_j   = np.zeros((self._ny, self._nx), dtype = np.double)
-        im1_j = np.zeros((self._ny, self._nx), dtype = np.double)
-        ip1_j = np.zeros((self._ny, self._nx), dtype = np.double)
-        i_jm1 = np.zeros((self._ny, self._nx), dtype = np.double)
-        i_jp1 = np.zeros((self._ny, self._nx), dtype = np.double)
+        # coefficients are potentially unique. 
 
-        # populate coefficients for interior points
-        inv2delta2 = 1.0/(2.0*self._delta*self._delta)
-        kappa_i_j   = self._kappa[1:-1,1:-1] # just views, should be efficient
-        kappa_ip1_j = self._kappa[2:  ,1:-1]
-        kappa_im1_j = self._kappa[ :-2,1:-1]
-        kappa_i_jp1 = self._kappa[1:-1,2:  ]
-        kappa_i_jm1 = self._kappa[1:-1, :-2]
-
-        i_j[1:-1,1:-1] = -inv2delta2*(4.0*kappa_i_j + kappa_im1_j + kappa_ip1_j + kappa_i_jm1 + kappa_i_jp1)
-        im1_j[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_im1_j)
-        ip1_j[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_ip1_j)
-        i_jm1[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_i_jm1)
-        i_jp1[1:-1,1:-1] = inv2delta2*(kappa_i_j + kappa_i_jp1)
-
-        # populate coeff for boundary at y=0
-        if self._bc_x0 == 'constant':
-            pass
-
-        # populate coeff for boundary at y=end
-        if self._bc_x1 == 'constant':
-            pass
-
-        # populate coeff for boundary at x=0
-        if self._bc_y0 == 'constant':
-            pass
-
-        # populate coeff for boundary at x=max
-        if self._bc_y1 == 'constant':
-            pass
-
-        # convert coeffs to vectors
-        i_j = np.ravel(i_j, order='C')
-        ip1_j = np.ravel(ip1_j, order='C')
-        im1_j = np.ravel(im1_j, order='C')
-        i_jp1 = np.ravel(i_jp1, order='C')
-        i_jm1 = np.ravel(i_jm1, order='C')
-
-        # NOTE: leaving boundary coefficients as 0 is equivalent to a constant
-        # boundary condition. I leave this for now and move on to test the
-        # whole pipeline.
-
-        # construct sparse coefficient matrix
-        dd = [];                       kk = [] 
-        # # center diagonal 
-        dd.append(i_j);                kk.append(0)
-        # # upper diagonal, last element wraps
-        dd.append(i_jp1[:-1]);         kk.append(1) 
-        dd.append(i_jp1[-1:]);         kk.append(-self._ny*self._nx+1)  
-        # # lower diagonal, first element wraps
-        dd.append(i_jm1[1:]);          kk.append(-1) 
-        dd.append(i_jm1[:1]);          kk.append(self._ny*self._nx-1)
-        # # outer upper diagonal, last nx elements wrap
-        dd.append(ip1_j[0:-self._nx]); kk.append(self._nx)
-        dd.append(ip1_j[-self._nx:]);  kk.append(-self._ny*self._nx+self._nx)
-        # # outer lower diagonal, first nx elements wrap 
-        dd.append(im1_j[self._nx:]);   kk.append(-self._nx)
-        dd.append(im1_j[:self._nx]);   kk.append(self._ny*self._nx-self._nx)
-        # # construct compressed-row matrix from diagonals 
-        self._A = scipy.sparse.diags(dd, offsets=kk, format="csr", dtype=np.double)
-
-
-        # TEST: populate matrix using for loops instead, this may be more clear and maintainable
+        # init variables
         A = scipy.sparse.lil_matrix((self._ny*self._nx, self._ny*self._nx), dtype=np.double) # lil format is fast to populate
         c = 1.0/(2.0*self._delta*self._delta)
         kappa = self._kappa # alias for convenience 
-        k = lambda row, col: row*self._nx+col # sub2ind
+        k = lambda row, col: row*self._nx+col # map subscripts (row, col) to linear index (k) in row-major order
 
-        # interior points
+        # populate interior points
         for i in range(1,self._ny-1):
             for j in range(1,self._nx-1):
                 A[k(i,j), k(i  ,j  )] = -c*(4.0*kappa[i,j]+kappa[i-1,j]+kappa[i+1,j]+kappa[i,j-1]+kappa[i,j+1])
@@ -184,7 +113,32 @@ class ftcs():
                 A[k(i,j), k(i  ,j-1)] = c*(kappa[i,j]+kappa[i  ,j-1]) 
                 A[k(i,j), k(i  ,j+1)] = c*(kappa[i,j]+kappa[i  ,j+1])
 
-        self._A = A.tocsr() # csr format is fast for matrix*vector
+        # populate boundary at y=0
+        i = 0
+        for j in range(0,self._nx):
+            if self._bc_y0 == 'constant':
+                pass # dhdt = 0 
+
+        # populate boundary at y=end
+        i = self._ny-1
+        for j in range(0,self._nx):
+            if self._bc_y1 == 'constant':
+                pass # dhdt = 0 
+
+        # populate boundary at x=0
+        j = 0
+        for j in range(0,self._ny):
+            if self._bc_x0 == 'constant':
+                pass # dhdt = 0 
+
+        # populate boundary at x=max
+        j = self._nx-1
+        for j in range(0,self._ny):
+            if self._bc_x1 == 'constant':
+                pass # dhdt = 0 
+
+        # store results in effcient format for matrix*vector product
+        self._A = A.tocsr()
 
     def run(self, run_time):
         """
