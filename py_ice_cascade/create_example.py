9527944f9eca40068839e150bb20a65d184b7e72
keithfma <keithfma@gmail.com>
2016-08-20 10:18:37 -0400

    Switched to constant filenames for examples, use netCDF4 clobber protection

--- py_ice_cascade/create_example.py
+++ py_ice_cascade/create_example.py
@@ -5,16 +5,17 @@ Console script with options to create input files for example cases
 import argparse
 import os
 import sys
+import netCDF4
 
-def no_clobber(filename):
-    """Abort overwriting existing files"""
-    if os.path.exists(filename):
-        print("create_example: file '{}' exists, aborting".format(filename))
-        sys.exit()  
-
-def hill_only(filename):
+def hill_only(clobber):
     """Create input file for hillslope diffusion only example case"""
-    print(filename)
+
+    # create/open new file
+    filename = 'hill_only.in.nc'
+    rootgrp = netCDF4.Dataset(filename, "w", format="NETCDF4", clobber=clobber)
+
+    
+
 
 def main():
     """Select case from command line arguments and generate input file"""
@@ -25,14 +26,13 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('case', type=str, choices=['hill_only'],
         help='example case to generate')
-    parser.add_argument('filename', type=str, default='example_in.nc',
-        help='example case input file name')
+    parser.add_argument('-c', '--clobber', action='store_true',
+        help='allow overwriting existing files')
     args = parser.parse_args()
 
     # create input file for selected example case
-    no_clobber(args.filename)
     if args.case == 'hill_only':
-        hill_only(args.filename)
+        hill_only(args.clobber)
 
 if __name__ == '__main__':
     main()
