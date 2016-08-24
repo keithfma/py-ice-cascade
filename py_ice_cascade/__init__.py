7b27f9f2a8f5aa13351ecca9ae81c6a698dc9364
keithfma <keithfma@gmail.com>
2016-08-23 23:35:16 -0400

    Completed unit tests for basic __init__ interface

--- py_ice_cascade/__init__.py
+++ py_ice_cascade/__init__.py
@@ -11,4 +11,5 @@ _author_email = 'keithfma@gmail.com'
 # load modules
 import py_ice_cascade.ice_cascade as ice_cascade
 import py_ice_cascade.hillslope as hillslope
+import py_ice_cascade.uplift as uplift
 import py_ice_cascade.example as example
--- py_ice_cascade/test_uplift.py
+++ py_ice_cascade/test_uplift.py
@@ -3,17 +3,26 @@ Unit tests for Python ICE-CASCADE tectonic uplift-subsidence model component
 """
 
 import unittest
+import numpy as np
 from py_ice_cascade import uplift 
 
 class linear_TestCase(unittest.TestCase):
 
     def test_uplift_dims(self):
         """Initial and final uplift dims must match"""
-        pass
+        uplift.linear(np.random.rand(10,10), np.random.rand(10,10), 0, 1)
+        self.assertRaises(ValueError, uplift.linear, np.random.rand(10,10), np.random.rand(11,11), 0, 1)
 
-    def test_time_bnds(self):
-        """Time bounds must be increasing scalars"""
-        pass  
+    def test_time_scalar(self):
+        """Time bounds must be scalars"""
+        uplift.linear(np.random.rand(10,10), np.random.rand(10,10), 0, 1)
+        self.assertRaises(ValueError, uplift.linear, np.random.rand(10,10), np.random.rand(10,10), [0,0], 1)
+        self.assertRaises(ValueError, uplift.linear, np.random.rand(10,10), np.random.rand(10,10), 0, [1,1])
+
+    def test_time_incr(self):
+        """Time bounds must be increasing"""
+        uplift.linear(np.random.rand(10,10), np.random.rand(10,10), 0, 1)
+        self.assertRaises(ValueError, uplift.linear, np.random.rand(10,10), np.random.rand(10,10), 1, 0)
 
 if __name__ == '__main__':
     unittest.main()
