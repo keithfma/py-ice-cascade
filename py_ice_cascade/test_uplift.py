168acab8e11c2cfecdc0bbbd0fed691b682e631a
Keith Ma <keith.ma@bostonfusion.com>
2016-08-24 11:50:06 -0400

    Added accuracy check unittest for uplift.linear

--- py_ice_cascade/test_uplift.py
+++ py_ice_cascade/test_uplift.py
@@ -24,5 +24,32 @@ class linear_TestCase(unittest.TestCase):
         uplift.linear(np.random.rand(10,10), np.random.rand(10,10), 0, 1)
         self.assertRaises(ValueError, uplift.linear, np.random.rand(10,10), np.random.rand(10,10), 1, 0)
 
+    def test_random_uplift(self):
+        """Check uplift rate and total uplift for random uplift field"""
+        nx = ny = 100
+        ui = np.random.rand(nx, ny)
+        uf = -ui
+        ti = 0.0
+        tf = 1.0
+        epsilon = 0.00001
+        model = uplift.linear(ui, uf, ti, tf)
+
+        # correct uplift rate at t = ti, (ti+tf)/2, tf ?
+        error_ui = np.abs(model.get_uplift_rate(ti)-ui)
+        self.assertTrue(np.all(error_ui < epsilon))
+
+        error_um = np.abs(model.get_uplift_rate((ti+tf)/2.0) - (ui+uf)/2.0)
+        self.assertTrue(np.all(error_um < epsilon))
+
+        error_uf = np.abs(model.get_uplift_rate(tf)-uf)
+        self.assertTrue(np.all(error_uf < epsilon))
+
+        # correct total uplift at t = ti, tf ?
+        error_uti = np.abs(model.get_uplift(ti, ti) - 0.0)
+        self.assertTrue(np.all(error_uti < epsilon))
+        
+        error_utf = np.abs(model.get_uplift(ti, tf) - 0.0)
+        self.assertTrue(np.all(error_utf < epsilon))
+
 if __name__ == '__main__':
     unittest.main()
